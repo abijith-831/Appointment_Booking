c77b7761fc07041af212a1a52144fac71d3b416c
Abhijith <abhijithasokan831@gmail.com>
2025-07-13 17:35:13 +0530

    portected route fixed

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
-// ProtectedRoute.tsx
 import { Navigate } from 'react-router-dom';
+import type { ReactNode } from 'react';
 
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
